d07eb5e1eba2a35b3e53c15e97320738291b4671
Kaminskyyy <87198204+Kaminskyyy@users.noreply.github.com>
2022-03-07 22:07:11 +0200

    add reply

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const oauth = {
 	consumer_key: '9EWItTB05s049qtfZWC7VImY2',
 	consumer_secret: 'YB0pxAQ7eGLNBJdK9EL46R3tq96CyJ5fQStZd0duEfqBx7Jppv',
 };
+
 let token_secret = null;
 
 const url = 'https://api.twitter.com/oauth/request_token';
@@ -26,10 +27,10 @@ app.use(express.static(join(__dirname,  '/public')));
 
 console.log(join(__dirname,  '/public'));
 
-app.get('/auth', (req, res) => {
+app.get('/auth', async (req, res) => {
 	request.post({ url, oauth }, (e, r, body) => {
 		const req_data = qs.parse(body);
-		//res.send(req_data);
+		
 		const uri = 'https://api.twitter.com/oauth/authenticate' + '?' + qs.stringify({oauth_token: req_data.oauth_token});
 
 		token_secret = req_data.oauth_token_secret;
@@ -38,7 +39,6 @@ app.get('/auth', (req, res) => {
 });
 
 app.get('/callback', (req, res) => {
-	//res.send(req.query);
 	oauth.token = req.query.oauth_token;
 	oauth.token_secret = token_secret;
 	oauth.verifier = req.query.oauth_verifier;
@@ -57,26 +57,53 @@ app.get('/callback', (req, res) => {
 	});
 });
 
-app.get('/user', (req, res) => {
-	console.log(req.query);
 
-	const url = 'https://api.twitter.com/2/users/by/username/' + req.query.screen_name;
-		
+
+app.get('/user', (req, res) => {
+
+	const url = 'https://api.twitter.com/2/users/by/username/' + req.query.screen_name + '?expansions=pinned_tweet_id';
+	
 	request.get({ url, oauth }, (e, r, user) => {
+		console.log(user);
 		res.send(user);
 	});
 });	
 
 app.get('/postTweet', (req, res) => {
 	let url = 'https://api.twitter.com/2/tweets';
-	//url += '?' + req.query.text;
 
 	const body = {
 		text: req.query.text
 	};
 
+	if (req.query.idToReply) {
+		body.reply = {
+			in_reply_to_tweet_id: req.query.idToReply,
+		};
+	}
+	
+
 	request.post({ url, oauth, body, json: true}, (e, r, body) => {
 		console.log(body);
+	});
+});
+
+app.get('/postReply', (req, res) => {
+	const url = 'http://localhost:2000/user?screen_name=' + req.query.screen_name;
+
+	request.get({ url, json: true }, (e, r, body) => {
+		const url = 'https://api.twitter.com/2/tweets/search/recent?query=' + encodeURIComponent('from:' + body.data.username);  
+		
+		request.get({ url, oauth, json:true }, (e, r, body) => {
+			
+			if (body.meta.result_count < 1) {
+				res.send({error: 'no tweets'});
+			} else {
+				//const url = 'http://localhost:2000/postTweet?'
+				request.get()
+				res.send();
+			}
+		});
 	});
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,11 @@ const userInput = document.getElementById('userInput');
 const tweetForm = document.getElementById('tweetForm');
 const tweetInput = document.getElementById('tweetInput');
 
+const replyToForm = document.getElementById('replyToForm');
+const replyToInput = document.getElementById('replyToInput');
+const replyTweetForm = document.getElementById('replyTweetForm');
+const replyTweetInput = document.getElementById('replyTweetInput');
+
 userForm.addEventListener('submit', (event) => {
 	event.preventDefault();
 
@@ -17,5 +22,16 @@ tweetForm.addEventListener('submit', (event) => {
 
 	fetch('/postTweet?text=' + tweetInput.value)
 		.then((response) => response.json())
-		.then((user) => console.log(user));
+		.then((tweet) => console.log(tweet));
+});
+
+replyTweetForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	fetch('/postReply?screen_name=' + replyToInput.value)
+		.then((response) => response.json())
+		.then((body) => console.log(body));
+
+	//console.log(replyToInput.value);
+	//console.log(replyTweetInput.value);
 });
